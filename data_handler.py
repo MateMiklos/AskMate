27b017a922f575528061ba7ee4d3e238e0bfae11
tejfi <adamfdeak@gmail.com>
2019-01-14 23:55:48 +0100

    Merge branch 'master' of https://github.com/CodecoolGlobal/super-sprinter-3000-python-tejfi
    # Conflicts:
    #	templates/list.html

--- data_handler.py
+++ data_handler.py
@@ -2,8 +2,9 @@ import csv
 import os
 
 DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
+DATA_FILE_PATH_answer = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'answer.csv'
 DATA_HEADER = ["id","submission_time","view_number","vote_number","title","message","image"]
-header = ['ID', 'Title', 'User Story', 'Acceptance Criteria', 'Business Value', 'Estimation', 'Status']
+header = ['ID', 'Submission Time', 'View Number', 'Vote Number', 'Title', 'Message', 'Image']
 
 
 def get_all_user_story():
--- server.py
+++ server.py
@@ -12,6 +12,21 @@ def route_list():
     print(user_stories)
     return render_template("list.html", user_stories=user_stories, header=header)
 
+
+@app.route('/story', methods=['POST', 'GET'])
+def user_story():
+    if request.method == 'POST':
+        user_storiezz = dict(request.form)
+        data_handler.append_csv(user_storiezz)
+        return redirect('/')
+
+    user_stories = data_handler.get_all_user_story()
+    header = data_handler.get_header()
+    # statuses = data_handler.get_statuses()
+    # id = data_handler.get_next_id()
+    return render_template('add.html', user_stories=user_stories, header=header, id=id)
+
+
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
