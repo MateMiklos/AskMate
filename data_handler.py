799bb74c8140321fb4d971c2b80b565c3e038e7e
ZalanLP <zalan.permenter@gmail.com>
2019-01-15 14:33:14 +0100

    Timestamp creator created

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import datetime
 
 DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
 DATA_FILE_PATH_answer = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'answer.csv'
@@ -17,3 +18,17 @@ def get_questions():
 
 def get_header():
     return header
+
+
+def append_csv(question):
+    with open('AskMate/sample_data/question.csvs') as csv_questions:
+        writer = csv.writer(csv_questions)
+        writer.writerows(question)
+
+
+def id_generator():
+    pass
+
+
+def create_timestamp():
+    return datetime.datetime.fromtimestamp(ts).strftime('%H-%M-%S %d:%m:%Y')
