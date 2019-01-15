9f5de8d66068a228686852798a484d9d5fb944a4
ZalanLP <zalan.permenter@gmail.com>
2019-01-15 13:45:26 +0100

    More name changes, display.html in the works

--- data_handler.py
+++ data_handler.py
@@ -3,19 +3,16 @@ import os
 
 DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
 DATA_FILE_PATH_answer = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'answer.csv'
-DATA_HEADER = ["id","submission_time","view_number","vote_number","title","message","image"]
 header = ['ID', 'Submission Time', 'View Number', 'Vote Number', 'Title', 'Message', 'Image']
 
 
-def get_all_user_story():
+def get_questions():
     with open(DATA_FILE_PATH) as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         questions = []
         for row in reader:
             questions.append(row)
     return questions
-
-
 
 
 def get_header():
--- server.py
+++ server.py
@@ -6,24 +6,30 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/list')
-def route_list():
-    questions = data_handler.get_all_user_story()
+def list_():
+    questions = data_handler.get_questions()
     header = data_handler.get_header()
     return render_template("list.html", questions=questions, header=header)
 
 
-@app.route('/add-question', methods=['POST', 'GET'])
-def user_story():
+@app.route('/ask-question', methods=['POST', 'GET'])
+def ask_question():
     if request.method == 'POST':
         user_storiezz = dict(request.form)
         data_handler.append_csv(user_storiezz)
         return redirect('/')
 
-    questions = data_handler.get_all_user_story()
+    questions = data_handler.get_questions()
     header = data_handler.get_header()
     # statuses = data_handler.get_statuses()
     # id = data_handler.get_next_id()
     return render_template('add.html', questions=questions, header=header, id=id)
+
+@app.route('/question/<question_id>')
+def display_question():
+    questions = data_handler.get_questions()
+    header = data_handler.get_header()
+    return render_template('display.html', questions=questions, header=header, id=id)
 
 
 if __name__ == '__main__':
