9a5718f221fdee37cbf3aacff7a90c78fc8ac6d8
MateMiklos <miklos.mate.91@gmail.com>
2019-01-30 11:20:58 +0100

    comments on answers feature

--- data_handler.py
+++ data_handler.py
@@ -1,13 +1,12 @@
 import csv, os, datetime
 import database_common
 from datetime import datetime
-DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
-DATA_FILE_PATH_answer = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/answer.csv'
+
+
 header = {'ID':'ID', 'Submission Time':'Submission Time', 'View Number': 'View Number', 'Vote Number':'Vote Number', 'Title':'Title', 'Message':'Message', 'Image':'Image'}
 DATA_HEADER = ["id", "submission_time", "view_number", "vote_number", "title", "message","image"]
 answers_header = ["id","submission_time","vote_number","question_id","message","image"]
 DATA_HEADER_ANSWER = ['ID', 'Submission Time','Question ID', 'Message', 'Image']
-
 
 
 @database_common.connection_handler
@@ -18,6 +17,7 @@ def get_questions(cursor):
     questions = cursor.fetchall()
     return questions
 
+
 @database_common.connection_handler
 def get_answers(cursor):
     cursor.execute("""
@@ -26,6 +26,7 @@ def get_answers(cursor):
     answers = cursor.fetchall()
 
     return answers
+
 
 @database_common.connection_handler
 def insert_question_table(cursor,view_number,vote_number, title,message,image):
@@ -36,16 +37,19 @@ def insert_question_table(cursor,view_number,vote_number, title,message,image):
     """,
         {'submission_time':submission_time,'view_number':view_number,'vote_number':vote_number,'title':title,'message':message,'image':image})
 
+
 @database_common.connection_handler
-def insert_answer_table(cursor,vote_number,question_id,message,image):
+def insert_answer_table(cursor, vote_number, question_id, message, image):
     submission_time = datetime.now()
     cursor.execute("""INSERT INTO answer(submission_time,vote_number, question_id,message,image)
        VALUES(%(submission_time)s,%(vote_number)s,%(question_id)s, %(message)s,%(image)s);
 
        """,
             {'submission_time': submission_time, 'vote_number': vote_number,'question_id':question_id,'message': message, 'image': image})
+
+
 @database_common.connection_handler
-def get_result_by_search(cursor,title):
+def get_result_by_search(cursor, title):
     cursor.execute("""
                             SELECT id,submission_time, view_number, vote_number,title,message FROM question
                             WHERE title LIKE  %(title)s
@@ -56,17 +60,16 @@ def get_result_by_search(cursor,title):
     return result
 
 
-
 def get_next_id():
     questions = get_questions()
     id = int(questions[-1]['id']) + 1
     return id
 
+
 def get_next_question_id():
     questions = get_answers()
     question_id = int(questions[-1]['id']) + 20
     return question_id
-
 
 
 def get_next_answer_id():
@@ -75,10 +78,9 @@ def get_next_answer_id():
     return answer_id
 
 
-
-
 def get_header():
     return header
+
 
 def get_answer_header():
     return DATA_HEADER_ANSWER
@@ -89,3 +91,55 @@ def upload_file():
         f = request.files['file']
         f.save(secure_filename(f.filename))
         return 'file uploaded successfully'
+
+
+@database_common.connection_handler
+def get_comments_by_answer_id(cursor, answer_id):
+    cursor.execute("""
+    SELECT message FROM comment
+    WHERE answer_id=%(answer_id)s;
+    """, {'answer_id': answer_id})
+    comments = cursor.fetchall()
+    return comments
+
+
+@database_common.connection_handler
+def get_answers_by_question_id(cursor, question_id):
+    cursor.execute("""
+                    SELECT * FROM answer
+                    WHERE question_id=%(question_id)s;
+    """, {'question_id': question_id})
+
+    answers = cursor.fetchall()
+    return answers
+
+
+@database_common.connection_handler
+def get_answer_by_id(cursor, id):
+    cursor.execute("""
+                    SELECT * FROM answer
+                    WHERE id=%(id)s;
+    """, {'id': id})
+
+    answer = cursor.fetchall()
+    return answer
+
+
+@database_common.connection_handler
+def get_question_by_id(cursor, id):
+    cursor.execute("""
+                    SELECT * FROM question
+                    WHERE id=%(id)s;
+                   """, {'id': id})
+
+    question = cursor.fetchall()
+    return question
+
+
+@database_common.connection_handler
+def insert_comment_table(cursor, answer_id, message):
+    submission_time = datetime.now()
+    cursor.execute("""INSERT INTO comment(submission_time, answer_id, message)
+       VALUES (%(submission_time)s,%(answer_id)s, %(message)s);
+       """, {'submission_time': submission_time, 'answer_id': answer_id, 'message': message})
+
--- server.py
+++ server.py
@@ -2,20 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 import data_handler
 
 
-
 app = Flask(__name__)
-
-
-
-
-
 
 
 @app.route('/')
 @app.route('/list')
 def index():
     return render_template("index.html")
-
 
 
 @app.route("/search", methods=["POST"])
@@ -33,12 +26,12 @@ def list_():
 @app.route('/ask-question', methods=['POST', 'GET'])
 def ask_question():
     if request.method == 'POST':
-        title=request.form.get('title')
-        view_number=request.form.get("view_number")
-        vote_number=request.form.get("vote_number", type=int)
-        message=request.form.get("message")
-        image=request.form.get("image")
-        data_handler.insert_question_table(view_number,vote_number,title,message,image)
+        title = request.form.get('title')
+        view_number = request.form.get("view_number")
+        vote_number = request.form.get("vote_number", type=int)
+        message = request.form.get("message")
+        image = request.form.get("image")
+        data_handler.insert_question_table(view_number, vote_number, title, message, image)
         return redirect('/')
 
     questions = data_handler.get_questions()
@@ -49,19 +42,13 @@ def ask_question():
 
 @app.route('/question/<id>', methods=['GET', 'POST'])
 def display_question(id):
-    answers = data_handler.get_answers()
-    questions = data_handler.get_questions()
+    answers = data_handler.get_answers_by_question_id(id)
+    questions = data_handler.get_question_by_id(id)
     header = data_handler.get_header()
     answers_header = data_handler.get_answer_header()
-    for num in questions:
-        if num["id"] == int(id):
-            questionz = num
 
-    for line in answers:
-        if line["question_id"] == int(id):
-            answers = line
-    return render_template('display.html', questionz=questionz, answers=answers,header=header, answers_header=answers_header)
-
+    return render_template('display.html', questions=questions, answers=answers, header=header,
+                           answers_header=answers_header)
 
 
 @app.route('/question/<question_id>/new-answer', methods=['POST', 'GET'])
@@ -70,29 +57,26 @@ def add_answer(question_id):
         vote_number=request.form["vote_number"]
         message=request.form["message"]
         image=request.form["image"]
-        data_handler.insert_answer_table(vote_number,question_id,message,image)
+        data_handler.insert_answer_table(vote_number, question_id, message, image)
         return redirect(url_for("display_question", id=question_id))
 
-
-    question_container = data_handler.get_questions()
-    for num in question_container:
-        if num["id"] == int(question_id):
-            question_number = num
-            print(num)
-    return render_template('add_answer.html', question_number=question_number, question_id=question_id)
+    question = data_handler.get_question_by_id(question_id)
+    return render_template('add_answer.html', question=question, question_id=question_id)
 
 
-@app.route('/question/<id>/vote-up')
-def vote_up(id):
-    question
-    vote_counter += 1
-    return vote_counter, render_template('display.html',questions=questions, questionz=questionz, answers = answers,header=header, id=id)
+@app.route('/answer/<id>/new-comment', methods=['POST', 'GET'])
+def comments_on_answers(id):
+    answer = data_handler.get_answer_by_id(id)
+    comments = data_handler.get_comments_by_answer_id(id)
+    question = data_handler.get_answers_by_question_id(id)
+    print(question)
+    header = ['Comments:']
+    if request.method == 'POST':
+        message = request.form["message"]
+        data_handler.insert_comment_table(id, message)
+        return redirect(url_for("display_question", id=id))
 
-
-@app.route('/vote-down/<question_id>')
-def vote_down(question_id):
-    vote_counter -= 1
-    return vote_counter, redirect('question/id')
+    return render_template('answer_comments.html', id=id, answer=answer, comments=comments, header=header, question=question)
 
 
 if __name__ == '__main__':
